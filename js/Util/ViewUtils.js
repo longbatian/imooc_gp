046feeaeb9bd20e12287dd9b14b48aa706b5d510
longbatian <425774702@qq.com>
2018-04-03 11:12:56 +0800

    403

--- js/Util/ViewUtils.js
+++ js/Util/ViewUtils.js
@@ -1,8 +1,22 @@
 /**
  * Created by ao on 2018/4/3.
  */
-export default class ViewUtils{
-    static getLeftButton(){
+import React,{Component} from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    StyleSheet,
+} from 'react-native';
 
+export default class ViewUtils{
+    static getLeftButton(callBack){
+        return <TouchableOpacity
+            style={{padding:8}}
+            onPress={callBack}
+        >
+            <Image style={{width:22,height:22}} source={require('../../res/images/ic_arrow_back_white_36pt.png')}/>
+        </TouchableOpacity>
     }
 }
