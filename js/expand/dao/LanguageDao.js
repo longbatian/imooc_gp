7135cc2e11659392304399fa0142ab48f8374d12
longbatian <425774702@qq.com>
2018-04-04 09:47:18 +0800

    404

---
+++ js/expand/dao/LanguageDao.js
@@ -0,0 +1,42 @@
+/**
+ * Created by ao on 2018/3/23.
+ */
+import React, {Component} from 'react';
+import {
+    AsyncStorage,
+} from 'react-native';
+import keys from '../../../res/data/keys.json';
+
+export var FlAG_LANGUAGE={flag_language:'flag_language_language',flag_key:'flag_language_key'};
+export default class LanguageDao{
+    constructor(flag){
+        this.flag=flag;
+    }
+    fetch(){
+        return new Promise((resolve,reject)=>{
+            AsyncStorage.getItem(this.flag,(error,result)=>{
+                if(error){
+                    reject(error);
+                }else{
+                    if (result){
+                        try{
+                            resolve(JSON.parse(result));
+                        }catch(e){
+                            reject(e)
+                        }
+
+                    }else{
+                        var data=this.flag===FlAG_LANGUAGE.flag_key?keys:null;
+                        this.save(data);
+                        resolve(data);
+                    }
+                }
+            })
+        })
+    }
+    save(data){
+        AsyncStorage.setItem(this.flag,JSON.stringify(data),(error)=>{
+
+        })
+    }
+}
--- js/pages/my/CustomKeyPage.js
+++ js/pages/my/CustomKeyPage.js
@@ -6,26 +6,75 @@ import {
     View,
     Text,
     StyleSheet,
+    TouchableOpacity,
+    ScrollView,
 } from 'react-native';
 import {Navigator} from 'react-native-deprecated-custom-components';
 import NavigationBar from '../../common/NavigationBar';
+import ViewUtils from '../../Util/ViewUtils';
+import LanguageDao,{FlAG_LANGUAGE} from '../../expand/dao/LanguageDao';
 
 export default class CustomKeyPage extends Component{
     constructor(props) {
         super(props);
-        this.state = {
-            tabShow: false
+        this.languageDao=new LanguageDao(FlAG_LANGUAGE.flag_key);
+        this.state={
+            dataArray:[]
         }
     };
+    componentDidMount(){
+        this.loadData();
 
+    }
+    loadData(){
+        this.languageDao.fetch()
+            .then(result=>{
+                this.setState({
+                    dataArray:result
+                })
+            })
+            .catch(error=>{
+                alert(error)
+            })
+    }
+    onSave(){
+        this.props.navigator.pop();
+    }
+    renderView(){
+        if(!this.state.dataArray||this.state.dataArray.length===0) return null;
+        let len=this.state.dataArray.length;
+        let views=[];
+        for(let i=0,l=len-2;i<l;i+=2){
+            views.push(
+                <View key={i}>
+                    <View style={styles.item}>
+                        <Text>{this.state.dataArray[i].name}</Text>
+                        <Text>{this.state.dataArray[i+1].name}</Text>
+                    </View>
+                    <View style={styles.line}></View>
+                </View>
+            )
+        }
+    }
 
     render(){
-        return <View>
+        let rightButton=<TouchableOpacity
+            onPress={()=>this.onSave()}
+        >
+            <View style={{margin:10}}>
+                <Text style={styles.title}>保存</Text>
+            </View>
+        </TouchableOpacity>
+        return <View style={styles.container}>
              <NavigationBar
                 title={'自定义标签'}
-                leftButton={}
+                leftButton={ViewUtils.getLeftButton(()=>this.onSave())}
+                rightButton={rightButton}
              />
-            <Text style={styles.tips}>自定义标签</Text>
+            <ScrollView>
+                {this.renderView()}
+
+            </ScrollView>
         </View>
     }
 }
@@ -35,6 +84,19 @@ const styles=StyleSheet.create({
     },
     tips:{
         fontSize:29
+    },
+    title:{
+        fontSize:20,
+        color:'white',
+
+    },
+    line:{
+        height:1,
+        backgroundColor:'black'
+    },
+    item:{
+        flexDirection:'row',
+        alignItems:'center'
     }
 })
 
