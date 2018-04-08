f60436dcab5b88f8453e428699e7595a8be3953f
longbatian <425774702@qq.com>
2018-04-08 09:21:24 +0800

    408

--- js/pages/my/CustomKeyPage.js
+++ js/pages/my/CustomKeyPage.js
@@ -8,95 +8,123 @@ import {
     StyleSheet,
     TouchableOpacity,
     ScrollView,
+    Image,
+
 } from 'react-native';
 import {Navigator} from 'react-native-deprecated-custom-components';
 import NavigationBar from '../../common/NavigationBar';
+import Checkbox from 'react-native-check-box'
 import ViewUtils from '../../Util/ViewUtils';
-import LanguageDao,{FlAG_LANGUAGE} from '../../expand/dao/LanguageDao';
+import LanguageDao, {FlAG_LANGUAGE} from '../../expand/dao/LanguageDao';
 
-export default class CustomKeyPage extends Component{
+export default class CustomKeyPage extends Component {
     constructor(props) {
         super(props);
-        this.languageDao=new LanguageDao(FlAG_LANGUAGE.flag_key);
-        this.state={
-            dataArray:[]
+        this.languageDao = new LanguageDao(FlAG_LANGUAGE.flag_key);
+        this.state = {
+            dataArray: []
         }
     };
-    componentDidMount(){
-        this.loadData();
 
+    componentDidMount() {
+        this.loadData();
     }
-    loadData(){
+
+    loadData() {
         this.languageDao.fetch()
-            .then(result=>{
+            .then(result=> {
                 this.setState({
-                    dataArray:result
+                    dataArray: result
                 })
             })
-            .catch(error=>{
+            .catch(error=> {
                 alert(error)
             })
     }
-    onSave(){
+
+    onSave() {
         this.props.navigator.pop();
     }
-    renderView(){
-        if(!this.state.dataArray||this.state.dataArray.length===0) return null;
-        let len=this.state.dataArray.length;
-        let views=[];
-        for(let i=0,l=len-2;i<l;i+=2){
+
+    renderView() {
+        if (!this.state.dataArray || this.state.dataArray.length === 0) return null;
+        let len = this.state.dataArray.length;
+        let views = [];
+        for (let i = 0, l = len - 2; i < l; i += 2) {
             views.push(
                 <View key={i}>
                     <View style={styles.item}>
                         <Text>{this.state.dataArray[i].name}</Text>
-                        <Text>{this.state.dataArray[i+1].name}</Text>
+                        <Text>{this.state.dataArray[i + 1].name}</Text>
                     </View>
                     <View style={styles.line}></View>
                 </View>
             )
         }
+        views.push(
+            <View key={len - 1}>
+                <View style={styles.item}>
+                    {len % 2 === 0 ? <Text>{this.state.dataArray[len - 2].name}</Text> : null}
+                    <Text>{this.state.dataArray[len - 1].name}</Text>
+                </View>
+                <View style={styles.line}></View>
+            </View>
+        )
+        return views;
+
     }
 
-    render(){
-        let rightButton=<TouchableOpacity
+    renderCheckbox(data) {
+        let leftText = data.name;
+        return (
+            <Checkbox onClick={()=>this.onClick(this)}
+                      leftText={leftText}
+                      checkendImage={<Image
+                          source={require('')}
+                      />}
+            />
+        )
+    }
+
+    render() {
+        let rightButton = <TouchableOpacity
             onPress={()=>this.onSave()}
         >
-            <View style={{margin:10}}>
+            <View style={{margin: 10}}>
                 <Text style={styles.title}>保存</Text>
             </View>
         </TouchableOpacity>
         return <View style={styles.container}>
-             <NavigationBar
+            <NavigationBar
                 title={'自定义标签'}
                 leftButton={ViewUtils.getLeftButton(()=>this.onSave())}
                 rightButton={rightButton}
-             />
+            />
             <ScrollView>
                 {this.renderView()}
-
             </ScrollView>
         </View>
     }
 }
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
     },
-    tips:{
-        fontSize:29
+    tips: {
+        fontSize: 29
     },
-    title:{
-        fontSize:20,
-        color:'white',
+    title: {
+        fontSize: 20,
+        color: 'white',
 
     },
-    line:{
-        height:1,
-        backgroundColor:'black'
+    line: {
+        height: 1,
+        backgroundColor: 'black'
     },
-    item:{
-        flexDirection:'row',
-        alignItems:'center'
+    item: {
+        flexDirection: 'row',
+        alignItems: 'center'
     }
 })
 
